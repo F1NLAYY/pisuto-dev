249c0495c571813247311b77459901bdf459e3e0
F1NLAYY <twentyfirststfeb@gmail.com>
2024-11-29 14:12:41 +0700

    try to fix deploy problem again

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js"
-import exp from "constants"
 
 if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY){
     throw new Error('Missing Supabase environment variables')
